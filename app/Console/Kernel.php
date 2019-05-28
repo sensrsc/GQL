cc2df2a47e7fec5fc928e71131e4ab7d23cbc05f
Sean Hsu <sean.hsu@kkday.com>
2019-05-28 16:39:51 +0800

    feat: add graphql

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
+// use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -22,11 +22,11 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+    // protected function schedule(Schedule $schedule)
+    // {
+    //     // $schedule->command('inspire')
+    //     //          ->hourly();
+    // }
 
     /**
      * Register the commands for the application.
